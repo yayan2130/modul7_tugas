1915305f8179792f0cd156165e855cb680f6b88b
yayan2130 <yayan2130@gmail.com>
2020-11-23 20:41:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/modul7_praktikum/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.modul7_praktikum
+
+import com.example.modul7_praktikum.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>(this,
+                R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController =
+            this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+}
